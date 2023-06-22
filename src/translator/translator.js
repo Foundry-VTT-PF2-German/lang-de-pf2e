8415d250e47e0cb98be1cb64ee858922e9dede18
MSAbaddon <66370966+MSAbaddon@users.noreply.github.com>
2023-06-23 01:13:20 +0200

    prettier-Format

--- src/translator/translator.js
+++ src/translator/translator.js
@@ -287,7 +287,7 @@ class Translator {
                     entry.name = originalName;
 
                     // Since Foundry V11 compendium links also include the item type - needs to get stripped
-                    arr[index] = game.babele.packs.get(itemCompendium.replace(".Item","")).translate(entry);
+                    arr[index] = game.babele.packs.get(itemCompendium.replace(".Item", "")).translate(entry);
 
                     // Remove dual language translations
                     if (arr[index].name.search("/") != -1) {
