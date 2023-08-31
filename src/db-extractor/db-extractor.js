f49e823c9cbf456207095ea09f776f0386e760af
MSAbaddon <66370966+MSAbaddon@users.noreply.github.com>
2023-09-01 01:26:49 +0200

    Update pack extraction

--- src/db-extractor/db-extractor.js
+++ src/db-extractor/db-extractor.js
@@ -1,52 +1,39 @@
 import { readFileSync, writeFileSync } from "fs";
-import { resolvePath } from "path-value";
-import { getZipContentFromURL } from "../helper/src/util/fileHandler.js";
-import { extractFiles, extractPackGroup, extractPackGroupList } from "../helper/src/pack-extractor/pack-extractor.js";
-import { sortObject } from "../helper/src/util/utilities.js";
+import { getZipContentFromURL, writeFiles } from "../helper/src/util/fileHandler.js";
+import { replaceProperties } from "../helper/src/util/utilities.js";
+import { extractPackGroupList } from "../helper/src/pack-extractor/pack-extractor.js";
 
 // Read config file
 const CONFIG = JSON.parse(readFileSync("./src/db-extractor/db-extractor-config.json", "utf-8"));
 
-// Initialize database
-const database = {
-    dictionary: {},
-    actorItemComparison: {},
-};
+// Replace linked mappings and savePaths with actual data
+replaceProperties(CONFIG.mappings, ["subMapping"], CONFIG.mappings);
+replaceProperties(CONFIG.packs, ["mapping"], CONFIG.mappings);
+replaceProperties(CONFIG.packs, ["savePath"], CONFIG.filePaths.packs);
 
 // Fetch assets from current pf2 release and get zip contents
 const packs = await getZipContentFromURL(CONFIG.filePaths.zipURL);
 
-// Extract data for mandatory ItemPacks and all other packs
-if (resolvePath(CONFIG, "packs.ItemPacks").exists) {
-    const packGroupConfig = {
-        name: "ItemPacks",
-        packConfig: CONFIG.packs.ItemPacks,
-        mappings: CONFIG.mappings,
-        savePath: CONFIG.filePaths.packs[CONFIG.packs.ItemPacks.savePath],
-    };
-    extractPackGroup(
-        packs.filter((pack) => CONFIG.packs.ItemPacks.packNames.includes(pack.fileName)),
-        database,
-        packGroupConfig
-    );
-    const packGroupListConfig = {
-        packGroupList: CONFIG.packs.OtherPacks,
-        mappings: CONFIG.mappings,
-        filePaths: CONFIG.filePaths.packs,
-    };
-    extractPackGroupList(packs, database, packGroupListConfig);
+// Extract data for all configured packs
+const extractedPackGroupList = extractPackGroupList(packs, CONFIG.packs);
 
-    // Extract and write i18n files
-    extractFiles(
-        packs.filter((pack) => CONFIG.i18nFiles.includes(`${pack.fileName}.${pack.fileType}`)),
-        CONFIG.filePaths.i18n
-    );
-} else console.error(`Mandatory Pack Group "ItemPacks" missing in config.`);
+// Write extracted packs to target directories
+Object.keys(extractedPackGroupList.extractedPackGroups).forEach((packGroup) => {
+    const path = CONFIG.packs[packGroup].savePath;
+    Object.keys(extractedPackGroupList.extractedPackGroups[packGroup]).forEach((pack) => {
+        writeFileSync(
+            `${path}/${pack}.json`,
+            JSON.stringify(extractedPackGroupList.extractedPackGroups[packGroup][pack], null, 2)
+        );
+    });
+});
 
-// Build the dictionary
-if (Object.keys(database.dictionary).length > 0) {
-    writeFileSync(
-        CONFIG.filePaths.dictionary,
-        JSON.stringify(sortObject(database.dictionary, { recursiveSort: true }), null, 2)
-    );
-}
+// Write dictionary to target directory
+writeFileSync(CONFIG.filePaths.dictionary, JSON.stringify(extractedPackGroupList.packGroupListDictionary, null, 2));
+
+// Extract and write i18n files
+writeFiles(
+    packs.filter((pack) => CONFIG.i18nFiles.includes(`${pack.fileName}.${pack.fileType}`)),
+    CONFIG.filePaths.i18n,
+    "i8n files"
+);
